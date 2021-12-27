2785d3942e296dbec7427b2f6ceb96359c44a3eb
huda <sadyahmed43@gmail.com>
2021-12-28 01:38:39 +1100

    five tables added

--- database/migrations/2021_12_25_124626_create_departments_table.php
+++ database/migrations/2021_12_25_124626_create_departments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDepartmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('fullname');
-            $table->string('username');
-            $table->string('password');
-            $table->timestamps();// created_at and updated_at
+            $table->string('name');
+            $table->string('descr');
+          
+            $table->timestamps();
+            
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('departments');
     }
 }
--- database/migrations/2021_12_27_135347_create_attachments_table.php
+++ database/migrations/2021_12_27_135347_create_attachments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAttachmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('fullname');
-            $table->string('username');
-            $table->string('password');
-            $table->timestamps();// created_at and updated_at
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -29,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attachments');
     }
 }
